4c4f3b37037bd446463abcd60e4f8da327b6daaf
LianZongQuan <2695106618@qq.com>
2022-09-15 08:45:45 +0800

    提交修改

--- ReactBaseApp/App.js
+++ ReactBaseApp/App.js
@@ -1,110 +1,43 @@
-/*
- * @Author: error: git config user.name && git config user.email & please set dead value or install git
- * @Date: 2022-08-30 15:25:50
- * @LastEditors: error: git config user.name && git config user.email & please set dead value or install git
- * @LastEditTime: 2022-08-30 18:02:17
- * @FilePath: \ReactBaseApp\App.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
 import {
-  Link,
-  Text,
-  HStack,
-  Center,
-  Heading,
-  Switch,
-  useColorMode,
-  NativeBaseProvider,
-  VStack,
-  Box,
-  Button,
-  AspectRatio,
-  Image,
-  Stack,
-  FormControl,
-  Select,
-  CheckIcon,
-  WarningOutlineIcon,
-  ScrollView,
-  Flex,
-  Spacer
+  Link, Text, HStack, Center, Heading, Switch, useColorMode, NativeBaseProvider, VStack, Box,
+  Button, AspectRatio, Image,  FormControl, Select, CheckIcon, WarningOutlineIcon, ScrollView,
+  Flex, Spacer
 } from 'native-base';
-
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Provider } from 'react-redux';
-import store
- from './redux/store';
-
-//  import HomeScreen from './src/components/HomeScreen/HomeScreen';
+import store from './redux/store';
 import HomeScreen from './src/components/HomeScreen/HomeScreen';
-// Color Switch Component
-function ToggleDarkMode() {
-  const {colorMode, toggleColorMode} = useColorMode();
-  return (
-    <HStack space={2} alignItems="center">
-      <Text>Dark</Text>
-      <Switch
-        isChecked={colorMode === 'light'}
-        onToggle={toggleColorMode}
-        aria-label={
-          colorMode === 'light' ? 'switch to dark mode' : 'switch to light mode'
-        }
-      />
-      <Text>Light</Text>
-    </HStack>
-  );
-}
-const App = () => {
+import Test1 from './src/components/test1';
+
+// const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator();
+function App() {
   return (
     <Provider store={store} >
+      <NativeBaseProvider >
+        <NavigationContainer>
 
+          <Stack.Navigator initialRouteName="主页">
+            <Stack.Screen name="主页"
+              options={{
+                  header: ({ navigation }) => { },
+                  title: '',
+              }}
+              component={HomeScreen}>
+            </Stack.Screen>
+            <Stack.Screen name="测试"
+              options={{
+                  header: ({ navigation }) => { },
+                  title: '',
+              }}
+              component={Test1}>
+            </Stack.Screen>
 
-    <NativeBaseProvider >
-
-    <HomeScreen></HomeScreen>
-
-
-
-      {/* <Box>
-        <Button testID='1'>asd</Button>
-      </Box> */}
-      {/* <Counter></Counter> */}
-      {/* <Center
-        _dark={{bg: 'blueGray.900'}}
-        _light={{bg: 'blueGray.50'}}
-        px={4}
-        flex={1}>
-        <VStack space={5} alignItems="center">
-          <NativeBaseIcon />
-          <Heading size="lg">Welcome to NativeBase</Heading>
-          <HStack space={2} alignItems="center">
-            <Text>Edit</Text>
-            <Box
-              px={2}
-              py={1}
-              _dark={{bg: 'blueGray.800'}}
-              _light={{bg: 'blueGray.200'}}>
-              App.js
-            </Box>
-            <Text>and save to reload.</Text>
-          </HStack>
-          <Link href="https://docs.nativebase.io" isExternal>
-            <Text color="primary.500" underline fontSize={'xl'}>
-              Learn NativeBase
-            </Text>
-          </Link>
-          <ToggleDarkMode />
-        </VStack>
-      </Center> */}
-    </NativeBaseProvider>
+          </Stack.Navigator>
+        </NavigationContainer>
+      </NativeBaseProvider>
     </Provider>
   );
 };
--- ReactBaseApp/src/components/HomeScreen/HomeScreen.js
+++ ReactBaseApp/src/components/HomeScreen/HomeScreen.js
@@ -1,73 +1,63 @@
 import React, { Component } from 'react';
 import {
   Link,Text,HStack,Center,Heading,Switch,useColorMode,NativeBaseProvider,VStack,Box,Button,AspectRatio,Image,Stagger,Stack,FormControl,isOpen,Select,CheckIcon,
-  children,Actionsheet,WarningOutlineIcon,ScrollView,Flex,Radio,Spacer,Input,AddIcon,Divider,Checkbox,sidebarItems,View
+  children,Actionsheet,WarningOutlineIcon,AlertDialog,Icon,ScrollView,Ionicons,Flex,Radio,Spacer,Input,AddIcon,Divider,Checkbox,sidebarItems,View, Container
 } from 'native-base';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useDisclose } from 'native-base';
 import HttpUtil from '../../utils/http';
 import { _ScrollView } from 'react-native';
 import { Progress } from 'native-base';
 
-const HomeScreen = () => {
-  const {
-    isOpen,
-    onOpen,
-    onClose
-  } = useDisclose();
-  return <Center _ScrollView = {{}}>
-      <Button onPress={onOpen}>Actionsheet</Button>
-      <Actionsheet  isOpen={isOpen} onClose={onClose}>
-        
-        <Actionsheet.Content  _dragIndicatorWrapperOffSet={{
-        py: '10'
-      }}>
-          <Box w="100%" h={60} px={4} justifyContent="center">
-            <Text fontSize="16" color="gray.500" _dark={{
-            color: 'gray.300'
-          }}>
-              Albums
-            </Text>
-          </Box>
-          <Actionsheet.Item>Delete</Actionsheet.Item>
-          <Actionsheet.Item>Share</Actionsheet.Item>
-          <Actionsheet.Item>Play</Actionsheet.Item>
-          <Actionsheet.Item>Favourite</Actionsheet.Item>
-        </Actionsheet.Content>
-      </Actionsheet>
-      {/* //rn实现少半圆效果 */}
-        <View style={{
-          overflow: 'hidden',//这行代码不可省略
-          top: 30,
-          backgroundColor: "#CBCBCB",
-          height: 18,
-          width: 18,
-          borderRadius: 9,
-          marginTop: 20
-        }}>
-          <View style={{ width: 6, height: 18, backgroundColor: 'rgba(34,171,148, 1.0)' }}></View>
-        </View>
+const Example = () => {
+  return(
+    <HStack  justifyContent="center">
+      <Spacer></Spacer>
+    <Center h="40" w="20" bg="primary.300" rounded="md" shadow={3} />
+    <Spacer></Spacer>
+    <Center h="40" w="20" bg="primary.500" rounded="md" shadow={3} />
+    <Spacer></Spacer>
+    <Center h="40" w="20" bg="primary.700" rounded="md" shadow={3} />
+    <Spacer></Spacer>
+  </HStack>
+  )
+};
 
-      <Box w="90%" maxW="400">
-        <VStack space="md">
-          <VStack mx="4" space="md">
-            <Progress colorScheme="primary" value={35} />
-            <Progress colorScheme="secondary" value={45} />
-            <Progress colorScheme="emerald" value={55} />
-            <Progress colorScheme="warning" value={65} />
-            <Progress colorScheme="light" value={99} />
-          </VStack>
-        </VStack>
-      </Box>
-    </Center>;
+const HomeScreen = ({navigation}) => {
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const onClose = () => setIsOpen(false);
+
+  const cancelRef = React.useRef(null);
+  return(
+    <View bg={"#ffffff"} style = {{width:"100%",height:"100%"}} alignItems="center">
+      <Center shadow="1" rounded="2xl" bg="white:alpha.20" style = {{marginTop:"10%",width:"90%",height:"30%"}}>
+        <HStack flexDirection={"row"} style = {{borderWidth:1}} >
+          <Text fontSize={"2xl"} h = {"10"} style = {{borderWidth:1,padding:0}}>当期日期：</Text>
+          <Select style = {{borderWidth:1}}  h = {"10"} minWidth="100" accessibilityLabel="Choose Service" placeholder="Choose Service" _selectedItem={{
+        bg: "teal.600",
+        endIcon: <CheckIcon size="5" />
+      }} mt={1} >
+          <Select.Item label="UX Research" value="ux" />
+          <Select.Item label="Web Development" value="web" />
+          <Select.Item label="Cross Platform Development" value="cross" />
+          <Select.Item label="UI Designing" value="ui" />
+          <Select.Item label="Backend Development" value="backend" />
+        </Select>
+        </HStack>
 
 
-}
-const test = () =>{
+      </Center>
+      <Center shadow="1" rounded="2xl" bg="white:alpha.20" style = {{marginTop:"10%",width:"90%",height:"30%"}}>
+      </Center>
+    </View>
+
+
+  ) 
+   
+
   
-}
 
-
-function Example() {
- 
 }
 export default HomeScreen;
